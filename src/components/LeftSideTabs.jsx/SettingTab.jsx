b605c13fd9b740cfedaf27d03cdbd93a8a0a829a
shinespark460 <jennifer0914H@outlook.com>
2025-11-11 23:23:25 +0530

    refactor: streamline SettingTab component by removing unused image dialog logic and updating state management

--- src/components/LeftSideTabs.jsx/SettingTab.jsx
+++ src/components/LeftSideTabs.jsx/SettingTab.jsx
@@ -13,24 +13,18 @@ import {
   Lock,
   MessageCircleQuestionMark,
 } from "lucide-react";
-// import useFormatDate from "../../hooks/useFormatDate";
-import ImagePreviewDialog from "../../lib/ImageModel";
 const SettingTab = () => {
   const { authUser } = useContext(AppContext);
+  const [formData, setFormData] = useState({
+    fullName: authUser?.fullName || "",
+    email: authUser?.email || "",
+    bio: authUser?.bio || "",
+    profilePic: authUser?.profilePic || "",
+    backgroundPic: authUser?.backgroundPic || "",
+  })
   // State to track if the dialog is open and which image URL to display
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [selectedImageUrl, setSelectedImageUrl] = useState(null);
   // Function that will be called on image click
-  const openImageDialog = (url) => {
-    setSelectedImageUrl(url); // Set the URL of the clicked image
-    setDialogOpen(true); // Open the dialog
-  };
 
-  // Function to close the dialog
-  const closeImageDialog = () => {
-    setDialogOpen(false);
-    setSelectedImageUrl(null); // Optional: clear the URL when closing
-  };
   const [open, setOpen] = useState({
     personal: false,
     themes: false,
@@ -49,7 +43,7 @@ const SettingTab = () => {
 
         {/* Overlay Title */}
         <div
-          onClick={() => openImageDialog(authUser?.backgroundPic || bgImage)}
+        
           className="absolute top-0 cursor-pointer left-0 w-full h-full bg-black/20 flex items-start justify-between px-4 py-3"
         >
           <h2 className="text-white text-xl font-semibold tracking-wide drop-shadow">
@@ -61,12 +55,9 @@ const SettingTab = () => {
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2">
           <div className="w-32 h-32 rounded-full border-4 border-white shadow-md overflow-hidden bg-gray-100 cursor-pointer">
             <img
-              src={authUser?.profilePic || assets.avatar_icon}
+              src={formData.profilePic || assets.avatar_icon}
               alt="profile"
               className="w-full h-full object-cover"
-              onClick={() =>
-                openImageDialog(authUser?.profilePic || assets.avatar_icon)
-              }
             />
           </div>
         </div>
@@ -84,44 +75,8 @@ const SettingTab = () => {
       </div>
 
       {/* Scrollable Section */}
-      <div className="flex-1 overflow-y-auto mt-3 pb-10">
-        {/* <h3 className="text-lg font-semibold text-gray-700  mb-2">About</h3>
-        <p className="text-sm text-gray-600 leading-relaxed">
-          {authUser?.bio || "Hey there! I am using Synk"}
-        </p> */}
+      <div className="flex-1 overflow-y-auto pb-10">
 
-        {/* <div className="mt-2">
-          <h3 className="text-lg font-semibold text-gray-700 mb-4">
-            Contact Info
-          </h3>
-          <p className="flex flex-row gap-4 items-center mb-2 text-gray-600">
-            <span>
-              <CircleUserRound size={20} />
-            </span>
-            <span>{authUser?.fullName || "Tony Stark"}</span>
-          </p>
-          <a
-            href={`mailto:${authUser?.email}`}
-            className="flex flex-row gap-4 items-center mb-2 text-gray-600"
-          >
-            <span>
-              <Mail size={20} />
-            </span>
-            <span>{authUser?.email || "Tony Stark"}</span>
-          </a>
-          <p className="flex flex-row gap-4 items-center mb-2 text-gray-600">
-            <span>
-              <History size={20} />
-            </span>
-            <span>{useFormatDate(authUser?.createdAt) || "Tony Stark"}</span>
-          </p>
-          <p className="flex flex-row gap-4 items-center mb-2 text-gray-600">
-            <span className="">
-              <SquarePen size={20} />
-            </span>
-            <span>{useFormatDate(authUser?.updatedAt) || "--_--_--"}</span>
-          </p>
-        </div> */}
         <div className="mt-2">
           {/* 1️⃣ Personal Info */}
           <div className="border-b-[1px] border-[#8b8b8b94] px-4">
@@ -129,19 +84,18 @@ const SettingTab = () => {
               onClick={() => setOpen({ ...open, personal: !open.personal })}
               className="w-full flex items-center justify-between  py-4 text-left  transition-all duration-200"
             >
-              <h3 className="font-medium text-gray-800">Personal Info</h3>
+              <h3 className="font-medium text-gray-800 flex flex-row items-center gap-2"><span><CircleUserRound size={20} /></span> Personal Info</h3>
               {open.personal ? (
                 <ChevronUp className="text-green-600 transition-transform duration-300" />
               ) : (
                 <ChevronDown className="text-gray-500 transition-transform duration-300" />
               )}
             </button>
-            <div
-              className={`overflow-hidden transition-all duration-500 ease-in-out ${
-                open.personal
+            <form
+              className={`overflow-hidden transition-all duration-500 ease-in-out ${open.personal
                   ? "max-h-40 opacity-100 py-3"
                   : "max-h-0 opacity-0"
-              }`}
+                }`}
             >
               <p className="text-sm text-gray-600">
                 Update your name, profile photo, or contact info.
@@ -149,7 +103,7 @@ const SettingTab = () => {
               <p className="text-sm text-gray-600">
                 Change your display details easily here.
               </p>
-            </div>
+            </form>
           </div>
 
           {/* 2️⃣ Themes */}
@@ -158,7 +112,7 @@ const SettingTab = () => {
               onClick={() => setOpen({ ...open, themes: !open.themes })}
               className="w-full flex items-center justify-between  py-4 text-left  transition-all duration-200"
             >
-              <h3 className="font-medium text-gray-800">Themes</h3>
+              <h3 className="font-medium text-gray-800 flex flex-row items-center gap-2"><span><Contrast size={20} /></span> Themes</h3>
               {open.themes ? (
                 <ChevronUp className="text-green-600 transition-transform duration-300" />
               ) : (
@@ -166,9 +120,8 @@ const SettingTab = () => {
               )}
             </button>
             <div
-              className={`overflow-hidden transition-all duration-500 ease-in-out ${
-                open.themes ? "max-h-40 opacity-100 py-3" : "max-h-0 opacity-0"
-              }`}
+              className={`overflow-hidden transition-all duration-500 ease-in-out ${open.themes ? "max-h-40 opacity-100 py-3" : "max-h-0 opacity-0"
+                }`}
             >
               <p className="text-sm text-gray-600">
                 Switch between light, dark, or system themes.
@@ -185,7 +138,7 @@ const SettingTab = () => {
               onClick={() => setOpen({ ...open, account: !open.account })}
               className="w-full flex items-center justify-between py-4 text-left  transition-all duration-200"
             >
-              <h3 className="font-medium text-gray-800">Security</h3>
+              <h3 className="font-medium text-gray-800 flex flex-row items-center gap-2"><span><Lock size={20} /></span> Security</h3>
               {open.account ? (
                 <ChevronUp className=" transition-transform duration-300" />
               ) : (
@@ -193,9 +146,8 @@ const SettingTab = () => {
               )}
             </button>
             <div
-              className={`overflow-hidden transition-all duration-500 ease-in-out ${
-                open.account ? "max-h-40 opacity-100 py-3" : "max-h-0 opacity-0"
-              }`}
+              className={`overflow-hidden transition-all duration-500 ease-in-out ${open.account ? "max-h-40 opacity-100 py-3" : "max-h-0 opacity-0"
+                }`}
             >
               <p className="text-sm text-gray-600">
                 Manage login sessions, password, and privacy settings.
@@ -212,7 +164,7 @@ const SettingTab = () => {
               onClick={() => setOpen({ ...open, help: !open.help })}
               className="w-full flex items-center justify-between  py-4 text-left transition-all duration-200"
             >
-              <h3 className="font-medium text-gray-800">Help</h3>
+              <h3 className="font-medium text-gray-800 flex flex-row items-center gap-2"><span><MessageCircleQuestionMark size={20} /></span> Help</h3>
               {open.help ? (
                 <ChevronUp className=" transition-transform duration-300" />
               ) : (
@@ -220,9 +172,8 @@ const SettingTab = () => {
               )}
             </button>
             <div
-              className={`overflow-hidden transition-all duration-500 ease-in-out ${
-                open.help ? "max-h-40 opacity-100 py-3" : "max-h-0 opacity-0"
-              }`}
+              className={`overflow-hidden transition-all duration-500 ease-in-out ${open.help ? "max-h-40 opacity-100 py-3" : "max-h-0 opacity-0"
+                }`}
             >
               <p className="text-sm text-gray-600">Find answers to FAQs.</p>
               <p className="text-sm text-gray-600">
@@ -232,11 +183,6 @@ const SettingTab = () => {
           </div>
         </div>
       </div>
-      <ImagePreviewDialog
-        open={dialogOpen}
-        imageUrl={selectedImageUrl}
-        onClose={closeImageDialog}
-      />
     </div>
   );
 };
